f8ad9dac04a3a2006d041efa8fc4fb34b4e3d8e1
loonsies <37041587+loonsies@users.noreply.github.com>
2025-12-10 00:21:55 +0100

    added u3 floors

--- Outer Ra'Kaznar [U2] - Sortie/settings.lua
+++ Outer Ra'Kaznar [U2] - Sortie/settings.lua
@@ -56,6 +56,61 @@ settings["mapRedirects"]["133_9"]["offsetX"] = 92;
 settings["mapRedirects"]["133_9"]["targetZone"] = 133;
 settings["mapRedirects"]["133_9"]["targetFloor"] = 4;
 
+-- Outer Ra'Kaznar [U3] - Sortie - Upper floors
+settings["mapRedirects"]["189_1"] = T {};
+settings["mapRedirects"]["189_1"]["offsetY"] = 32;
+settings["mapRedirects"]["189_1"]["offsetX"] = 92;
+settings["mapRedirects"]["189_1"]["targetZone"] = 133;
+settings["mapRedirects"]["189_1"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_2"] = T {};
+settings["mapRedirects"]["189_2"]["offsetY"] = 32;
+settings["mapRedirects"]["189_2"]["offsetX"] = 92;
+settings["mapRedirects"]["189_2"]["targetZone"] = 133;
+settings["mapRedirects"]["189_2"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_3"] = T {};
+settings["mapRedirects"]["189_3"]["offsetY"] = 32;
+settings["mapRedirects"]["189_3"]["offsetX"] = 92;
+settings["mapRedirects"]["189_3"]["targetZone"] = 133;
+settings["mapRedirects"]["189_3"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_4"] = T {};
+settings["mapRedirects"]["189_4"]["offsetY"] = 32;
+settings["mapRedirects"]["189_4"]["offsetX"] = 92;
+settings["mapRedirects"]["189_4"]["targetZone"] = 133;
+settings["mapRedirects"]["189_4"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_5"] = T {};
+settings["mapRedirects"]["189_5"]["offsetY"] = 32;
+settings["mapRedirects"]["189_5"]["offsetX"] = 92;
+settings["mapRedirects"]["189_5"]["targetZone"] = 133;
+settings["mapRedirects"]["189_5"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_6"] = T {};
+settings["mapRedirects"]["189_6"]["offsetY"] = 32;
+settings["mapRedirects"]["189_6"]["offsetX"] = 92;
+settings["mapRedirects"]["189_6"]["targetZone"] = 133;
+settings["mapRedirects"]["189_6"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_7"] = T {};
+settings["mapRedirects"]["189_7"]["offsetY"] = 32;
+settings["mapRedirects"]["189_7"]["offsetX"] = 92;
+settings["mapRedirects"]["189_7"]["targetZone"] = 133;
+settings["mapRedirects"]["189_7"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_8"] = T {};
+settings["mapRedirects"]["189_8"]["offsetY"] = 32;
+settings["mapRedirects"]["189_8"]["offsetX"] = 92;
+settings["mapRedirects"]["189_8"]["targetZone"] = 133;
+settings["mapRedirects"]["189_8"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["189_9"] = T {};
+settings["mapRedirects"]["189_9"]["offsetY"] = 32;
+settings["mapRedirects"]["189_9"]["offsetX"] = 92;
+settings["mapRedirects"]["189_9"]["targetZone"] = 133;
+settings["mapRedirects"]["189_9"]["targetFloor"] = 4;
+
 -- Outer Ra'Kaznar [U2] - Sortie - Basement floors
 settings["mapRedirects"]["133_10"] = T {};
 settings["mapRedirects"]["133_10"]["offsetY"] = 48;
@@ -98,3 +153,48 @@ settings["mapRedirects"]["133_16"]["offsetY"] = 48;
 settings["mapRedirects"]["133_16"]["offsetX"] = 56;
 settings["mapRedirects"]["133_16"]["targetZone"] = 133;
 settings["mapRedirects"]["133_16"]["targetFloor"] = 10;
+
+-- Outer Ra'Kaznar [U3] - Sortie - Basement floors
+settings["mapRedirects"]["189_10"] = T {};
+settings["mapRedirects"]["189_10"]["offsetY"] = 48;
+settings["mapRedirects"]["189_10"]["offsetX"] = 56;
+settings["mapRedirects"]["189_10"]["targetZone"] = 133;
+settings["mapRedirects"]["189_10"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["189_11"] = T {};
+settings["mapRedirects"]["189_11"]["offsetY"] = 48;
+settings["mapRedirects"]["189_11"]["offsetX"] = 56;
+settings["mapRedirects"]["189_11"]["targetZone"] = 133;
+settings["mapRedirects"]["189_11"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["189_12"] = T {};
+settings["mapRedirects"]["189_12"]["offsetY"] = 48;
+settings["mapRedirects"]["189_12"]["offsetX"] = 56;
+settings["mapRedirects"]["189_12"]["targetZone"] = 133;
+settings["mapRedirects"]["189_12"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["189_13"] = T {};
+settings["mapRedirects"]["189_13"]["offsetY"] = 48;
+settings["mapRedirects"]["189_13"]["offsetX"] = 56;
+settings["mapRedirects"]["189_13"]["targetZone"] = 133;
+settings["mapRedirects"]["189_13"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["189_14"] = T {};
+settings["mapRedirects"]["189_14"]["offsetY"] = 48;
+settings["mapRedirects"]["189_14"]["offsetX"] = 56;
+settings["mapRedirects"]["189_14"]["targetZone"] = 133;
+settings["mapRedirects"]["189_14"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["189_15"] = T {};
+settings["mapRedirects"]["189_15"]["offsetY"] = 48;
+settings["mapRedirects"]["189_15"]["offsetX"] = 56;
+settings["mapRedirects"]["189_15"]["targetZone"] = 133;
+settings["mapRedirects"]["189_15"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["189_16"] = T {};
+settings["mapRedirects"]["189_16"]["offsetY"] = 48;
+settings["mapRedirects"]["189_16"]["offsetX"] = 56;
+settings["mapRedirects"]["189_16"]["targetZone"] = 133;
+settings["mapRedirects"]["189_16"]["targetFloor"] = 10;
+
+
